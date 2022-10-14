75d3ced557624ba91cef56d0735a33d4671aae7f
Olayinka.Dev <aremuolayinka02@gmail.com>
2022-10-14 16:25:07 +0100

    still checking media queries

--- src/components/header/SearchNav.js
+++ src/components/header/SearchNav.js
@@ -8,72 +8,62 @@ import SearchSVG from "../../assets/icons/icon_search.svg";
 import { devices } from "../../utils/theme";
 
 const Wrapper = styled.div`
-  @media screen and (${devices.desktop}) {
-    display: flex;
-    align-items: center;
-    flex-grow: 1;
-    height: 100%;
-    background-color: transparent;
-  }
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  height: 100%;
+  background-color: transparent;
 
-  @media screen and (${devices.mobile}) {
+  @media only screen and (${devices.mobile}) {
     display: none;
   }
 `;
 
 const DirectionArrows = styled.img`
-  @media screen and (${devices.desktop}) {
-    width: 38px;
-    height: 31px;
-    margin-left: 10px;
-    cursor: pointer;
-  }
+  width: 38px;
+  height: 31px;
+  margin-left: 10px;
+  cursor: pointer;
 
-  @media screen and (${devices.mobile}) {
+  @media only screen and (${devices.mobile}) {
     display: none;
   }
 `;
 
 const SearchIcon = styled.img`
-  @media screen and (${devices.desktop}) {
-    position: absolute;
-    width: 18px;
-    height: 18px;
-    z-index: 10;
-    left: 10px;
-    top: 10px;
-  }
+  position: absolute;
+  width: 18px;
+  height: 18px;
+  z-index: 10;
+  left: 10px;
+  top: 10px;
 
-  @media screen and (${devices.mobile}) {
+  @media only screen and (${devices.mobile}) {
     display: none;
   }
 `;
 
 const SearchInput = styled.input`
-  @media screen and (${devices.desktop}) {
-    max-width: 471px;
-    border: 1px solid #d9d9d9;
-    background-color: transparent;
-    font-family: "Bw Modelica";
-    color: #d9d9d9;
-    padding: 10px 40px 10px 40px;
-    font-size: 1rem;
-  }
+  max-width: 471px;
+  border: 1px solid #d9d9d9;
+  background-color: transparent;
+  font-family: "Bw Modelica";
+  color: #d9d9d9;
+  padding: 10px 40px 10px 40px;
+  font-size: 1rem;
 
-  @media screen and (${devices.mobile}) {
+  @media only screen and (${devices.mobile}) {
     display: none;
   }
 `;
 
 const SearchContainer = styled.div`
-  @media screen and (${devices.desktop}) {
-    position: relative;
-    margin: 0;
-    padding: 0;
-    margin-left: 15px;
-  }
+  position: relative;
+  margin: 0;
+  padding: 0;
+  margin-left: 15px;
 
-  @media screen and (${devices.mobile}) {
+  @media only screen and (${devices.mobile}) {
     display: none;
   }
 `;
