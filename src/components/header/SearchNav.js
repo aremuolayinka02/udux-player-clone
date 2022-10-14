2c2a344a56e479711561ca9ba1cdc72f4e16b108
Olayinka.Dev <aremuolayinka02@gmail.com>
2022-10-14 16:27:42 +0100

    pushin gmedia query again

--- src/components/header/SearchNav.js
+++ src/components/header/SearchNav.js
@@ -14,7 +14,7 @@ const Wrapper = styled.div`
   height: 100%;
   background-color: transparent;
 
-  @media only screen and (${devices.mobile}) {
+  @media only screen and (${devices.mobile}) and (${devices.mobileS}) {
     display: none;
   }
 `;
